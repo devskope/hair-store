d466763fd0d5c1c3f49c701dcb20be371e4c0f6e
devskope <ope.oguntoye@protonmail.com>
2020-07-27 04:58:11 +0100

    [#36] implement user login - add login mutation - implement login

--- pages/account/login.jsx
+++ pages/account/login.jsx
@@ -1,6 +1,9 @@
 import { makeStyles } from '@material-ui/core';
+import { useMutation } from '@apollo/react-hooks';
+import { useRouter } from 'next/router';
 
 import AuthForm from '../../components/auth/AuthForm';
+import { LOGIN } from '../../lib/graphql/mutations/users';
 
 export const useStyles = makeStyles((theme) => ({
   page: {
@@ -11,10 +14,25 @@ export const useStyles = makeStyles((theme) => ({
 
 const Login = () => {
   const classes = useStyles();
+  const router = useRouter();
+
+  const [login, { loading, error }] = useMutation(LOGIN);
+
+  const handleLogin = async (formData) => {
+    try {
+      await login({ variables: formData });
+      router.push('/');
+    } catch (error) {}
+  };
 
   return (
     <div className={`full-height ${classes.page}`}>
-      <AuthForm mode='login' />
+      <AuthForm
+        mode='login'
+        onSubmit={handleLogin}
+        loading={loading}
+        postError={error}
+      />
     </div>
   );
 };
